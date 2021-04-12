b888255d743a00cb456c61c973fc2f51438c1a6b
GBKS <chri@sto.ph>
2021-04-12 10:38:03 +0200

    Added preview image

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
                 args[0].title = "Bitcoin icons";
                 args[0].description = "Interface icons for Bitcoin applications, free to use.";
                 args[0].locale = "en_US";
-                args[0].canonical = "https://bitcoin-icons.com";
-                args[0].preview = "https://bitcoin-icons.com/bitcoin-icon-set.jpg";
+                args[0].canonical = "https://bitcoinicons.com";
+                args[0].preview = "https://bitcoinicons.com/bitcoin-icons.jpg";
                 return args;
             })
     }
